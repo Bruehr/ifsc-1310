7e7e041155316d381512056dde20953beb8e3530
Brian Ruehr <143096850+Bruehr@users.noreply.github.com>
2024-02-21 12:47:15 +0000

    3-2

--- ifsc3300/ex3-2/test_scores.js
+++ ifsc3300/ex3-2/test_scores.js
@@ -1,6 +1,7 @@
 "use strict";
 
 const scores = [];
+let highestScore = 0;
 
 // use do-while loop to get the scores from the user
 let score = 0;
@@ -17,15 +18,24 @@ do {
 }
 while(score != -1);
 
-if (scores.length > 0) {
-    // use a for-in loop to add each score to total, and display score
+if (scores.length > 0) {   
     let total = 0;
     for (let i in scores) {
         total = total + scores[i];
+        if (scores[i] > highestScore) {
+            highestScore = scores[i];
+        }
         document.write(`<p>Score ${parseInt(i) + 1}: ${scores[i]}</p>`);
     }
-
-    //calculate and display the average
+  
     const average = parseInt(total/scores.length);
     document.write(`<p>Average score is ${average}</p>`);
+
+   
+    // for (let val of scores) {
+    //     if (val > highestScore) {
+    //         highestScore = val;
+    //     }
+    // }
+    document.write(`<p>Highest score is ${highestScore}</p>`)
 }
