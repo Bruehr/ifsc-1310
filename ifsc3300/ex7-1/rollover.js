ed9c3eea0c80a666641d1821680bda73260caf19
Brian Ruehr <143096850+Bruehr@users.noreply.github.com>
2024-03-06 15:36:21 +0000

    7-1

--- ifsc3300/ex7-1/rollover.js
+++ ifsc3300/ex7-1/rollover.js
@@ -9,20 +9,10 @@ document.addEventListener("DOMContentLoaded", () => {
     // preload images
     var links = $("#image_list").querySelectorAll("a");
 
-    // attach mouseover and mouseout events for each image
-    for ( let link of links) {
-        image1.addEventListener("mouseover", () => {
-
-        });
-        image1.addEventListener("mouseout", () => {
-
-        });
-        image2.addEventListener("mouseover", () => {
-
-        });
-        image2.addEventListener("mouseout", () => {
-
-        });           
+    for (let links of links) {
+        const image = new Image();
+        image.src = link.href;
     }
 
-});
+    
+
